0763097d624e28a9626d9eb36d8e0a78c2cfa016
monologue31 <monologue31@gmail.com>
2013-03-19 14:25:48 +0900

    20130319 04

---
+++ FriendNews.rb
@@ -0,0 +1,16 @@
+require 'thread'
+require 'nntpserver'
+require 'nntpfeeds'
+
+#Initialize globle parameter
+$fns_queue = SizedQueue.new(100)
+$fns_path = "/usr/local/bin/FriendNews"
+$fns_host = ""
+
+#Start Server
+server  = FriendNews::NNTPServer.new(11119)
+server.start
+
+#Start Newfeeds
+feeds   = FriendNews::NNTRFeeds.new
+feeds.run
