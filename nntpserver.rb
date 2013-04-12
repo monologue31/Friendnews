3e414f67785e9da3405b086798087df760e068bd
monologue31 <monologue31@gmail.com>
2013-04-12 16:59:17 +0900

    20120412 -1

--- nntpserver.rb
+++ nntpserver.rb
@@ -124,7 +124,7 @@ module FriendNews
         p message
         File.write("#{$fns_path}/article/#{message["Tag"]}}/#{message["Message_id"]}",self.to_str(message))
 
-        $fns_queue.push("192.168.83.146!#{message["Message_id"},#{message["Tag"]}")
+        $fns_queue.push("192.168.83.146!#{message["Message_id"]},#{message["Tag"]}")
 	  		return message["Message_id"],message["Tag"]
       when /(?i)ihave/
         tag = msg_str.scan(/Tag\s*:\s*.*\n/)[0].split(/\s*:\s*/)[1].chomp
