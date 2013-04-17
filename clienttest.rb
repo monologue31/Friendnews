60ea0a9e89731057a4c75bd589a955fd895856dc
monologue31 <monologue31@gmail.com>
2013-04-17 18:10:57 +0900

    20120417 -1

--- clienttest.rb
+++ clienttest.rb
@@ -1,5 +1,5 @@
 require 'nntpclient'
 
 host = FriendNews::NNTPClient.new(119)
-host.connect("192.168.83.143")
+host.connect("192.168.83.145")
 host.trans_file("POST",file_path = "test",msg_id = nil,tag = nil)
