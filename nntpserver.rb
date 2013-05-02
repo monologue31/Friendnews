1a95ce582546c861f213e1b8b72a188d6f269f2d
monologue31 <monologue31@gmail.com>
2013-05-02 19:27:33 +0900

    20120502 -1

--- nntpserver.rb
+++ nntpserver.rb
@@ -141,6 +141,8 @@ module FriendNews
 	  		#add Date
 	  		message["Date"] = Time.now.to_s unless message.key?("Date")
 
+        p message
+
         File.open("#{$fns_path}/article/#{message["Tag"]}/#{message["Message_id"]}","w") do |f|
           f.write self.to_str(message)
         end
