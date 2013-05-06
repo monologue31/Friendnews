728b723df6b6a88d85012d00165b872257bade4e
monologue31 <monologue31@gmail.com>
2013-05-06 14:15:57 +0900

    20120506 -1

--- nntpserver.rb
+++ nntpserver.rb
@@ -56,12 +56,10 @@ module FriendNews
           next unless line
           cmd,param = line.split(/\s+/,2)
           case cmd
-          when "MODE READER"
+          when "MODE"
             p cmd
-            if true
+            if pararm == "READER"
               @socket.puts("200 Hello,you can post")
-            else
-              @socket.puts("201 Hello,you can't post")
             end
           when /(?i)post/
             stat_code += 40
