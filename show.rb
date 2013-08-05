d4274471cfe367ff31cf059bb404d5af875d6211
monologue31 <monologue31@gmail.com>
2013-08-05 20:57:06 +0900

    20130805

--- show.rb
+++ show.rb
@@ -18,3 +18,27 @@ fnstags.each_key{|k|
   p fnstags[k]
 }
 p "---------------"
+
+junk = DBM::open("db/junk",0666)
+p "---------------"
+junk.each_key{|k|
+  p k
+  p junk[k]
+}
+p "---------------"
+
+all = DBM::open("db/all",0666)
+p "---------------"
+all.each_key{|k|
+  p k
+  p all[k]
+}
+p "---------------"
+
+test = DBM::open("db/test",0666)
+p "---------------"
+test.each_key{|k|
+  p k
+  p test[k]
+}
+p "---------------"
