adb655a9c82ff5be99c46486b7bc240ef580e686
monologue31 <monologue31@gmail.com>
2013-08-05 20:00:50 +0900

    20130805

--- show.rb
+++ show.rb
@@ -1,10 +1,20 @@
+require 'dbm'
 history = DBM::open("db/history",0666)
-art = DBM::open("article/music/article_number",0666)
 
-put "show history"
+p "show history"
+p "---------------"
 history.each_key{|k|
   p k
   p history[k]
 }
+p "---------------"
 
+p "show tag"
 
+fnstags = DBM::open("db/fnstags",0666)
+p "---------------"
+fnstags.each_key{|k|
+  p k
+  p fnstags[k]
+}
+p "---------------"
