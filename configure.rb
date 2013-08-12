4e59671974b2c2671be40c76b135fff6c6d54d88
monologue31 <monologue31@gmail.com>
2013-08-12 16:37:56 +0900

    20130812

--- configure.rb
+++ configure.rb
@@ -7,32 +7,32 @@ FileUtils.mkpath("article")
 FileUtils.mkpath("tmp")
 FileUtils.mkpath("db/tags")
 #set header
-header = DBM::open("db/header",0666)
-header.clear
-header["1"] = "Date"
-header["2"] = "From"
-header["3"] = "Message-ID"
-header["4"] = "Subject"
-header["5"] = "Newsgroups"
-header["6"] = "Path"
-header["7"] = "Expires"
-header["8"] = "Organization"
-header["9"] = "Reply-To"
-header["10"] = "Lines"
-header["11"] = "Signature"
-header["12"] = "Followup-To"
-header["13"] = "References"
-header["14"] = "Keywords"
-header["15"] = "Summary"
-header["16"] = "Distribution"
-header["17"] = "User-Agent"
-header["18"] = "MIME-Version"
-header["19"] = "Content-Type"
-header["20"] = "Content-Transfer-Encoding"
-header["21"] = "Control"
-header["22"] = "Xref"
-header["23"] = "Msg-Sign"
-header["24"] = "Body"
+headers = DBM::open("db/headers",0666)
+headers.clear
+headers["1"] = "Date"
+headers["2"] = "From"
+headers["3"] = "Message-ID"
+headers["4"] = "Subject"
+headers["5"] = "Newsgroups"
+headers["6"] = "Path"
+headers["7"] = "Expires"
+headers["8"] = "Organization"
+headers["9"] = "Reply-To"
+headers["10"] = "Lines"
+headers["11"] = "Signature"
+headers["12"] = "Followup-To"
+headers["13"] = "References"
+headers["14"] = "Keywords"
+headers["15"] = "Summary"
+headers["16"] = "Distribution"
+headers["17"] = "User-Agent"
+headers["18"] = "MIME-Version"
+headers["19"] = "Content-Type"
+headers["20"] = "Content-Transfer-Encoding"
+headers["21"] = "Control"
+headers["22"] = "Xref"
+headers["23"] = "Msg-Sign"
+headers["24"] = "Body"
 
 #clear history
 history = DBM::open("db/history",0666)
