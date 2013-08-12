f32c4c1ef1a216e5ddc4484ce205f1ea46e13f2e
monologue31 <monologue31@gmail.com>
2013-08-12 17:05:34 +0900

    20130812

--- nntpserver.rb
+++ nntpserver.rb
@@ -195,7 +195,7 @@ module FriendNews
       msg["Newsgroups"].split(",").each do |t|
         tags << t if active.has_key?(t)
       end
-      tags << "all" unless tags
+      tags << "all" unless tags.nil?
       p tags
       while 1
         msg["Message-ID"] = "<#{UUIDTools::UUID.random_create().to_s}@#{msg["From"].split("\s")[0]}>"
