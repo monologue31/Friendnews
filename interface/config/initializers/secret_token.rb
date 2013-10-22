6ed0151b6c536e1d0052606eb7e34716526f476a
monologue31 <monologue31@gmail.com>
2013-10-22 12:04:02 +0900

    20131022

---
+++ interface/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Interface::Application.config.secret_token = '663e4bbae6b8432b0d3fdcbffef3ba2ba53aa2eb85ff43f3688868148da5f32b7e95741089067e34bc11ed62d9f3247657db3b548849786bf0c64a12b5797dc5'
