55fe43e0b68e277b289f9a2cccb0d47433ecb70c
monologue31 <monologue31@gmail.com>
2014-08-19 14:29:25 +0900

    20140819

--- fnsserver.rb
+++ fnsserver.rb
@@ -173,7 +173,7 @@ module FriendNews
 
         #p "Path,Expires,Date"
         msg["Path"] = $fns_conf["host"]
-        msg["Expires"] = Time.now.since $fns_conf["expires"].dyas
+        msg["Expires"] = Time.now.since $fns_conf["expires"].to_i.days
         msg["Date"] = Time.now.to_s unless msg.key?("Date")
         msg["Signature"] = $fns_conf["signature"]
         msg["Distribution"] = "global" unless msg["Distribution"]
