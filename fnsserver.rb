b57c8545bca6eac1bd5e891a86bfaaf3e2ddfb65
monologue31 <monologue31@gmail.com>
2014-08-19 14:43:08 +0900

    20140819

--- fnsserver.rb
+++ fnsserver.rb
@@ -5,7 +5,7 @@ require 'uuidtools'
 require "fileutils"
 require 'openssl'
 require 'base64'
-require 'active_support'
+require 'active_support/time'
 
 module FriendNews
 
@@ -173,7 +173,7 @@ module FriendNews
 
         #p "Path,Expires,Date"
         msg["Path"] = $fns_conf["host"]
-        msg["Expires"] = Time.now.since $fns_conf["expires"].to_i.days
+        msg["Expires"] = Time.now + $fns_conf["expires"].to_i.days
         msg["Date"] = Time.now.to_s unless msg.key?("Date")
         msg["Signature"] = $fns_conf["signature"]
         msg["Distribution"] = "global" unless msg["Distribution"]
